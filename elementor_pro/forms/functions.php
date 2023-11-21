8b359c612d4f8ee3c9ed104e1423b8edcc6f0b3f
Sajid Islam <sajidislam@gmail.com>
2023-11-21 10:26:26 -0500

    Update functions.php
    Added comments

--- elementor_pro/forms/functions.php
+++ elementor_pro/forms/functions.php
@@ -34,27 +34,30 @@ add_action( 'wp_enqueue_scripts', 'ChildHelloElementor_enqueue_child_styles' );
 // as the functions.php of the (child)theme.
 
 add_action('elementor_pro/forms/validation', function($record, $ajax_handler) {
-
-	//before uncommenting the debug code below, make sure you turn on WP_DEBUG in wp-config.php
+/* ******************************** */
+/* ******************************** */
+	//before uncommenting the debug code (aka error_log)below, 
+	//make sure you turn on WP_DEBUG in wp-config.php & set these params
 	//define('WP_DEBUG', true);
 	//define('WP_DEBUG_LOG', true);
 	//define('WP_DEBUG_DISPLAY', false);
 	//@ini_set('display_errors', 0);
-        //error_log('This is a debug message from forms validation');
-   
-	$form_name = $record->get_form_settings('form_name');
-   	// error_log('Form Name: ' . $form_name);	
+/* ******************************** */
+/* ******************************** */  
+   //error_log('This is a debug message from forms validation');
+   $form_name = $record->get_form_settings('form_name');
+   //error_log('Form Name: ' . $form_name);	
 	
-   	// Ensure our custom functions are loaded.
-    	require_once get_stylesheet_directory() . '/elementor-form-custom-functions.php';
+   // Ensure our custom functions are loaded.
+    require_once get_stylesheet_directory() . '/elementor-form-custom-functions.php';
 
-   	
-   	if ($form_name == "12345") {
-     		validateForm($record,$ajax_handler);
-    	}
-   	else {
-      	// Add an error message
-    		$ajax_handler->add_error_message('Error: Form name is not valid or a match was not found');
-    	}	
-	return;	
+	if ($form_name == "12345") {
+        validateForm($record,$ajax_handler);
+    }
+	else {
+  		// Add an error message
+    	$ajax_handler->add_error_message('Error: Form name is not valid or a match was not found');
+    }	
+	return;
 }, 10, 2);
+
